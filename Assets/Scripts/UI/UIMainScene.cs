f535e7fbe67ffac4ce7b459bfe81c9acd20b2762
Uzay Adıgüzel <uzay.adiguzel@hotmail.com>
2021-09-15 11:06:50 +0300

    Save and Load
    +Color Change
    +Save
    +Load

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+    
+    public void BackScene()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
